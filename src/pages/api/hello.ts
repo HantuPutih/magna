61c37ff42480ded69f1eb3f4b925e878dd1f5873
HantuPutih <faridjuliano.fj@gmail.com>
2025-06-02 16:36:05 +0800

    comments

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -4,7 +4,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 type Data = {
   name: string;
 };
-
+// proxy to back end here
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
